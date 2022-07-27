0f244344f3bb6947e05ca94e72aebebf559111f3
SstealzZ <antoine.veber@epitech.eu>
2022-07-27 23:44:06 +0200

    Add version 1.1

--- calculate.py
+++ calculate.py
@@ -1,6 +1,10 @@
 import  sys, math, timeit, csv, datetime, date
 from math import *
 
+def get_date_of_birth(value):
+    d = datetime.datetime(int(date.get_year(value)), int(date.get_month(value)), int(date.get_day(value)))
+    return (d.date())
+
 def get_days_alive(value):
     tmp = datetime.datetime.today()
     d = datetime.datetime(int(date.get_year(value)), int(date.get_month(value)), int(date.get_day(value)))
@@ -18,3 +22,47 @@ def get_years_alive(value):
     d = datetime.datetime(int(date.get_year(value)), int(date.get_month(value)), int(date.get_day(value)))
     c = tmp - d
     return (int (c.days / 365))
+
+def get_date_of_death(opt, value):
+    ttd = 0
+    if (opt == "-m"):
+        ttd = 79
+    if (opt == "-f"):
+        ttd = 85
+    d = datetime.datetime(int(date.get_year(value)) + ttd, int(date.get_month(value)), int(date.get_day(value)))
+    return (d.date())
+
+def get_days_remaining(opt, value):
+    ttd = 0
+    if (opt == "-m"):
+        ttd = 79
+    if (opt == "-f"):
+        ttd = 85
+    db = tmp = datetime.datetime.today()
+    dd = datetime.datetime(int(date.get_year(value)) + ttd, int(date.get_month(value)), int(date.get_day(value)))
+    c = dd - db
+    return (int(c.days))
+
+def get_weeks_remaining(opt, value):
+    ttd = 0
+    if (opt == "-m"):
+        ttd = 79
+    if (opt == "-f"):
+        ttd = 85
+    db = tmp = datetime.datetime.today()
+    dd = datetime.datetime(int(date.get_year(value)) + ttd, int(date.get_month(value)), int(date.get_day(value)))
+    c = dd - db
+    return (int(c.days / 7))
+
+def get_years_remaining(opt, value):
+    ttd = 0
+    if (opt == "-m"):
+        ttd = 79
+    if (opt == "-f"):
+        ttd = 85
+    db = tmp = datetime.datetime.today()
+    dd = datetime.datetime(int(date.get_year(value)) + ttd, int(date.get_month(value)), int(date.get_day(value)))
+    c = dd - db
+    return (int(c.days / 365))
+
+    
--- printer.py
+++ printer.py
@@ -1,8 +1,28 @@
 import calculate
 def terminal_printer(gender, date):
+    print("--------------------------------------------------------------------------")
+
+    print("Date of birth:")
+    print(calculate.get_date_of_birth(date))
+    
+    print("--------------------------------------------------------------------------")
+    
     print("Days alive:")
     print(calculate.get_days_alive(date))
     print("Weeks alive:")
     print(calculate.get_weeks_alive(date))
     print("Years alive:")
     print(calculate.get_years_alive(date))
+
+    print("--------------------------------------------------------------------------")
+
+    print("Estimated date of death:")
+    print(calculate.get_date_of_death(gender, date))
+    print("Estimated days Remaining")
+    print(calculate.get_days_remaining(gender, date))
+    print("Estimated weeks Remaining")
+    print(calculate.get_weeks_remaining(gender, date))
+    print("Estimated years Remaining")
+    print(calculate.get_years_remaining(gender, date))
+
+    print("--------------------------------------------------------------------------")
